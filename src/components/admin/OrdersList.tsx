ac4ca55acac69f2d815016976c608601a65775f7
FUSEINI ABDUL WAHAB <abdulwahabfuseini78@gmail.com>
2026-05-09 03:48:00 +0000

    OrderList Adjusted

--- src/components/admin/OrdersList.tsx
+++ src/components/admin/OrdersList.tsx
@@ -13,7 +13,7 @@ const OrdersList = () => {
   }, []);
 
   return (
-    <div className="grid grid-cols-1 pb-20 gap-0p-4 md:grid-cols-2 lg:grid-cols-3">
+    <div className="grid grid-cols-1 gap-4 p-4 pb-20 md:grid-cols-2 lg:grid-cols-3">
       {orders.map((order: any) => (
         <div
           key={order.id}
